05b7009b8ccceebc6b93671428a0763a2e396abd
Pedro A. Hortas <pah@ucodev.org>
2015-04-29 10:54:46 +0100

    v0.00-skeleton-04

--- include/pipc.h
+++ include/pipc.h
@@ -3,7 +3,7 @@
  * @brief Portable IPC Library
  *        Public Library interface header
  *
- * Date: 27-04-2015
+ * Date: 29-04-2015
  * 
  * Copyright 2015 Pedro A. Hortas (pah@ucodev.org)
  *
@@ -30,18 +30,26 @@
 #include <stdio.h>
 
 /* Types */
-typedef long pipcd_t;
+typedef unsigned short pipck_t;
+
+/* Structures */
+typedef struct pipcd_struct {
+	int d;		/* IPC descriptor */
+	pipck_t key;
+	size_t msgmax;
+	size_t msgsize;
+	mode_t mode;
+} pipcd_t;
 
 /* Prototypes */
-pipcd_t pipc_create(const char *key, size_t msgmax, size_t msgsize, mode_t mode);
-pipcd_t pipc_register(const char *key, long id, mode_t mode);
-int pipc_send(pipcd_t pipcd, long src_id, long dst_id, const char *msg, size_t count);
-int pipc_recv(pipcd_t pipcd, long id, char *msg, size_t count);
-int pipc_send_nowait(pipcd_t pipcd, long src_id, long dst_id, const char *msg, size_t count);
-int pipc_recv_nowait(pipcd_t pipcd, long id, char *msg, size_t count);
-int pipc_unregister(pipcd_t pipcd);
-int pipc_destroy(pipcd_t pipcd);
-int pipc_unlink(const char *key);
+pipcd_t *pipc_master_register(pipck_t key, long id, size_t msgmax, size_t msgsize, mode_t mode);
+pipcd_t *pipc_slave_register(pipck_t key, long id, size_t msgmax, size_t msgsize, mode_t mode);
+int pipc_send(pipcd_t *pipcd, long src_id, long dst_id, const char *msg, size_t count);
+int pipc_recv(pipcd_t *pipcd, long id, char *msg, size_t count);
+int pipc_send_nowait(pipcd_t *pipcd, long src_id, long dst_id, const char *msg, size_t count);
+int pipc_recv_nowait(pipcd_t *pipcd, long id, char *msg, size_t count);
+int pipc_master_unregister(pipcd_t *pipcd);
+int pipc_slave_unregister(pipcd_t *pipcd);
 
 #endif
 
--- src/pipc.c
+++ src/pipc.c
@@ -1,53 +1,110 @@
+/**
+ * @file pipc.c
+ * @brief Potable IPC Library (libpipc)
+ *        Public Library interface
+ *
+ * Date: 29-04-2015
+ * 
+ * Copyright 2015 Pedro A. Hortas (pah@ucodev.org)
+ *
+ * This file is part of libpall.
+ *
+ * libpall is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libpall is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libpall.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
 #include <time.h>
 
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 #include "pipc.h"
+#include "mm.h"
 
-pipcd_t pipc_create(const char *key, size_t msgmax, size_t msgsize, mode_t mode) {
+static pipcd_t *_pipc_create(
+	pipck_t key,
+	long id,
+	size_t msgmax,
+	size_t msgsize,
+	mode_t mode,
+	int flags)
+{
+	int errsv = 0;
+	pipcd_t *pipcd = NULL;
+
+	/* Allocate IPC descriptor structure */
+	if (!(pipcd = mm_alloc(sizeof(pipcd_t))))
+		return NULL;
+
+	/* Create the IPC engine */
+	if ((pipcd->d = msgget((key_t) key, flags | mode)) < 0) {
+		errsv = errno;
+		mm_free(pipcd);
+		errno = errsv;
+		return NULL;
+	}
+
+	/* Set the IPC parameters */
+	pipcd->key = key;
+	pipcd->msgmax = msgmax;
+	pipcd->msgsize = msgsize;
+	pipcd->mode = mode;
+
+	/* All good */
+	return pipcd;
+}
+
+pipcd_t *pipc_master_register(pipck_t key, long id, size_t msgmax, size_t msgsize, mode_t mode) {
+	return _pipc_create(key, id, msgmax, msgsize, mode, IPC_CREAT);
+}
+
+pipcd_t *pipc_slave_register(pipck_t key, long id, size_t msgmax, size_t msgsize, mode_t mode) {
+	return _pipc_create(key, id, msgmax, msgsize, mode, 0);
+}
+
+int pipc_send(pipcd_t *pipcd, long src_id, long dst_id, const char *msg, size_t count) {
 	errno = ENOSYS;
 	return -1;
 }
 
-pipcd_t pipc_register(const char *key, long id, mode_t mode) {
+int pipc_recv(pipcd_t *pipcd, long id, char *msg, size_t count) {
 	errno = ENOSYS;
 	return -1;
 }
 
-int pipc_send(pipcd_t pipcd, long src_id, long dst_id, const char *msg, size_t count) {
+int pipc_send_nowait(pipcd_t *pipcd, long src_id, long dst_id, const char *msg, size_t count) {
 	errno = ENOSYS;
 	return -1;
 }
 
-int pipc_recv(pipcd_t pipcd, long id, char *msg, size_t count) {
+int pipc_recv_nowait(pipcd_t *pipcd, long id, char *msg, size_t count) {
 	errno = ENOSYS;
 	return -1;
 }
 
-int pipc_send_nowait(pipcd_t pipcd, long src_id, long dst_id, const char *msg, size_t count) {
+int pipc_master_unregister(pipcd_t *pipcd) {
 	errno = ENOSYS;
 	return -1;
 }
 
-int pipc_recv_nowait(pipcd_t pipcd, long id, char *msg, size_t count) {
-	errno = ENOSYS;
-	return -1;
-}
-
-int pipc_unregister(pipcd_t pipcd) {
-	errno = ENOSYS;
-	return -1;
-}
-
-int pipc_destroy(pipcd_t pipcd) {
-	errno = ENOSYS;
-	return -1;
-}
-
-int pipc_unlink(const char *key) {
+int pipc_slave_unregister(pipcd_t *pipcd) {
 	errno = ENOSYS;
 	return -1;
 }
